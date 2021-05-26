019e7372db963408467d444c76b0849e38d541d9
DevDams <ad.aimedesire@gmail.com>
2021-05-26 23:00:05 +0000

    start design home contact section

--- pages/index.vue
+++ pages/index.vue
@@ -64,14 +64,14 @@
           <h1 class="text-myblack text-6xl uppercase text-right">I love build or rebuild thing with code</h1>
         </div>
         <div class="home-para text-right mr-16 mt-8">
-          <p class="text-2xl">
+          <p class="text-2xl font-medium">
             Take a look at some of the thing i've done with passion
           </p>
         </div>
         <div class="home-project-area mt-20">
           <div class="area-1 flex">
             <div class="project-title w-1/2 flex">
-              <h3 class="text-lg uppercase ml-32">MAD.AC agency website clone</h3>
+              <h3 class="text-lg font-medium uppercase ml-32">MAD.AC agency website clone</h3>
               <svg
               class="ml-3 -mt-2"
               version="1.1"
@@ -95,12 +95,12 @@
               </svg>
             </div>
             <div class="project-image w-1/2">
-              <img class="pr-16" src="~/assets/images/mad.png" alt="">
+              <img class="pr-16" src="~/assets/images/mad_.png" alt="">
             </div>
           </div>
           <div class="area-2 mt-24 flex">
             <div class="project-image w-1/2">
-              <img class="pl-16" src="~/assets/images/pinky.png" alt="">
+              <img class="pl-16" src="~/assets/images/pinky_.png" alt="">
             </div>
             <div class="project-title w-1/2 flex">
               <svg
@@ -124,11 +124,11 @@
                   </g>
                 </g>
               </svg>
-              <h3 class="text-lg uppercase">Bloc note app</h3>
+              <h3 class="text-lg font-medium uppercase">Bloc note app</h3>
             </div>
           </div>
           <div class="home-project-more text-center mt-28">
-            <nuxt-link to="/work" class="px-16 py-6 bg-myblack text-white font-medium uppercase">
+            <nuxt-link to="/work" class="relative px-16 py-6 text-black font-bold uppercase">
               See more
             </nuxt-link>
           </div>
@@ -136,7 +136,23 @@
       </div>
       <!-- Contact section -->
       <div data-scroll-section class="home relative container mx-auto w-screen">
-        <h1>Contact me fast</h1>
+        <div class="home-contact-title mt-20">
+          <h1 class="text-6xl font-black uppercase pl-20">Let's get in touch</h1>
+        </div>
+        <div class="home-contact-eamil text-center mt-28">
+          <p class="text-2xl uppercase">Email :</p>
+          <p class="text-6xl font-semibold mt-6">
+            <a href="mailto:ad.aimedesire@gmail.com">
+              ad.aimedesire@gmail.com
+            </a>
+          </p>
+        </div>
+        <div class="home-contact-eamil text-center mt-16">
+          <p class="text-2xl uppercase">Phone :</p>
+          <p class="text-6xl font-semibold mt-6">
+            <a href="tel:+2250778812111">+225 07 78 81 2111</a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -166,8 +182,27 @@ body.dark .home {
 }
 
 body.dark .home-project-more a {
-  background-color: #e0e0e0;
-  color: black;
+  color: #e0e0e0;
+}
+
+body.dark .home-project-more a::after, body.dark .home-project-more a::before {
+  content: '';
+  position: absolute;
+  border: 2px solid #e0e0e0;
+  width: calc(100% - 4px);
+  height: calc(100% - 4px);
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+  transition: transform .3s ease;
+}
+
+body.dark .home-project-more a:hover::after {
+  transform: translate(-4px, -4px);
+}
+
+body.dark .home-project-more a:hover::before {
+  transform: translate(4px, 4px);
 }
 
 h1 {
@@ -176,7 +211,6 @@ h1 {
 
 p {
   font-family: 'Lato', sans-serif;
-  font-weight: 500;
 }
 
 .landing {
@@ -210,5 +244,29 @@ p {
   width: 30px;
   height: 30px;
   transform: rotate(-90deg);
+}
+
+.home-project-more a::after, .home-project-more a::before {
+  content: '';
+  position: absolute;
+  border: 2px solid black;
+  width: calc(100% - 4px);
+  height: calc(100% - 4px);
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+  transition: transform .3s ease;
+}
+
+.home-project-more a:hover::after {
+  transform: translate(-4px, -4px);
+}
+
+.home-project-more a:hover::before {
+  transform: translate(4px, 4px);
+}
+
+.home-contact-title a {
+  font-family: 'Krona One', sans-serif;
 }
 </style>
