55168c6f1c0dc32ba72faeea6d3e5e282ce0df68
DevDams <ad.aimedesire@gmail.com>
2021-06-02 17:26:52 +0000

    design and make work page responsive

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -14,17 +14,17 @@
       </div>
       <div class="prject_row">
         <!-- row 1 -->
-        <div class="row_1 container mx-auto flex w-screen">
+        <div class="row_1 container mx-auto flex flex-col md:flex-row w-screen">
           <!-- Project 1 -->
-          <div data-scroll-section class="project_1 relative mt-28 pr-8">
+          <div data-scroll-section class="project_1 relative pr-0 mt-28 md:pr-4 lg:pr-8">
             <div class="project_1_title">
               <h1 class="text-3xl uppercase pb-4">MAD agence website clone</h1>
             </div>
             <div class="project_1_desc relative mt-4">
-              <p class="font-light">
+              <p class="font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
               </p>
-              <a href="" class="absolute flex text-xl font-bold left-0 top-20">
+              <a href="https://ai-mad-clown.netlify.app" class="absolute flex text-xl font-medium left-0 top-20 lg:mt-8 md:mt-14">
                 <span class="pr-3">See It</span>
                 <svg
                 version="1.1"
@@ -50,15 +50,15 @@
             </div>
           </div>
           <!-- Project 2 -->
-          <div data-scroll-section class="project_2 relative mt-72 pl-8">
+          <div data-scroll-section class="project_2 relative pl-0 mt-28 sm:mt-32 md:mt-72 lg:pl-8">
             <div class="project_2_title">
               <h1 class="text-3xl uppercase pb-4">Spotify web player clone</h1>
             </div>
             <div class="project_2_desc relative mt-4">
-              <p class="font-light">
+              <p class="font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
               </p>
-              <a href="" class="absolute flex text-xl font-bold right-0 top-20">
+              <a href="https://spoti-spoti.netlify.app" class="absolute flex text-xl font-medium right-0 top-20 lg:mt-8 md:mt-14">
                 <span class="pr-3">See It</span>
                 <svg
                 version="1.1"
@@ -85,17 +85,17 @@
           </div>
         </div>
         <!-- row 2 -->
-        <div class="row_2 container mx-auto flex w-screen">
+        <div class="row_2 container mx-auto flex flex-col md:flex-row w-screen">
           <!-- Project 3 -->
-          <div data-scroll-section class="project_3 relative mt-28 pr-8">
+          <div data-scroll-section class="project_3 relative pr-0 mt-28 md:pr-4 lg:pr-8">
             <div class="project_3_title">
               <h1 class="text-3xl uppercase pb-4">Quizz App</h1>
             </div>
             <div class="project_3_desc relative mt-4">
-              <p class="font-light">
+              <p class="font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
               </p>
-              <a href="" class="absolute flex text-xl font-bold left-0 top-20">
+              <a href="https://qui-zz.netlify.app" class="absolute flex text-xl font-medium left-0 top-20 lg:mt-8 md:mt-14">
                 <span class="pr-3">See It</span>
                 <svg
                 version="1.1"
@@ -121,15 +121,15 @@
             </div>
           </div>
           <!-- Project 4 -->
-          <div data-scroll-section class="project_4 relative mt-72 pl-8">
+          <div data-scroll-section class="project_4 relative mt-28 pl-0 sm:mt-32 md:mt-72 lg:pl-8">
             <div class="project_4_title">
               <h1 class="text-3xl uppercase pb-4">Weather App</h1>
             </div>
             <div class="project_4_desc relative mt-4">
-              <p class="font-light">
+              <p class="font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
               </p>
-              <a href="" class="absolute flex text-xl font-bold right-0 top-20">
+              <a href="https://ai-weather.netlify.app" class="absolute flex text-xl font-medium right-0 top-20 lg:mt-8 md:mt-14">
                 <span class="pr-3">See It</span>
                 <svg
                 version="1.1"
@@ -156,17 +156,51 @@
           </div>
         </div>
         <!-- row 3 -->
-        <div class="row_3 container mx-auto flex w-screen">
+        <div class="row_3 container mx-auto flex flex-col md:flex-row w-screen">
           <!-- Project 5 -->
-          <div data-scroll-section class="project_5 relative w-1/2 mt-28 pr-8">
+          <div data-scroll-section class="project_5 relative mt-28 pr-0 md:pr-4 lg:pr-8">
             <div class="project_5_title">
-              <h1 class="text-3xl uppercase pb-4">MAD agence website clone</h1>
+              <h1 class="text-3xl uppercase pb-4">Pinky Note</h1>
             </div>
             <div class="project_5_desc relative mt-4">
-              <p class="font-light">
+              <p class="font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
               </p>
-              <a href="" class="absolute flex text-xl font-bold left-0 top-20">
+              <a href="https://pinky-notes.netlify.app" class="absolute flex text-xl font-medium left-0 top-20 lg:mt-8 md:mt-14">
+                <span class="pr-3">See It</span>
+                <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 200.981 200.981"
+                style="enable-background:new 0 0 200.981 200.981;"
+                xml:space="preserve">
+                  <g>
+                    <g>
+                      <g>
+                        <polygon
+                        style="fill:#010002;"
+                        points="17.511,10.264 129.068,10.264 129.068,0 0.007,0 0.007,129.068 10.257,129.068
+                        10.271,17.515 193.72,200.981 200.974,193.727"/>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <!-- Project 6 -->
+          <div data-scroll-section class="project_6 relative mt-28 sm:mt-32 pl-0 md:mt-72 lg:pl-8">
+            <div class="project_6_title">
+              <h1 class="text-3xl uppercase pb-4">Task App</h1>
+            </div>
+            <div class="project_6_desc relative mt-4">
+              <p class="font-normal">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in et nulla consectetur cum doloremque vitae aliquam, expedita numquam alias deserunt ad porro sunt perspiciatis sint, omnis nam praesentium vero!
+              </p>
+              <a href="https://ai-task.netlify.app/" class="absolute flex text-xl font-medium right-0 top-20 lg:mt-8 md:mt-14">
                 <span class="pr-3">See It</span>
                 <svg
                 version="1.1"
@@ -244,8 +278,12 @@ body.dark .project_5 h1 {
   border-bottom: 1px solid #e0e0e0;
 }
 
+body.dark .project_6 h1 {
+  border-bottom: 1px solid #e0e0e0;
+}
+
 .prject_row {
-  height: 1500px;
+  height: 1700px;
 }
 
 .intro_title h1 {
@@ -277,15 +315,84 @@ body.dark .project_5 h1 {
   font-family: 'Krona One', sans-serif;
 }
 
+.project_6 h1 {
+  border-bottom: 1px solid black;
+  font-family: 'Krona One', sans-serif;
+}
+
 a svg {
   width: 20px;
   transform: rotate(180deg);
 }
 
+@media (max-width: 767px) {
+  a {
+    left: 0;
+  }
+}
+
 @media (max-width: 640px) {
   #target {
     width: 92%;
-    margin: auto;
+    margin: 0 auto;
+  }
+
+  .prject_row {
+    height: 1800px;
+  }
+
+  .row_1, .row_2, .row_3 {
+    width: 100%;
+  }
+}
+
+@media (max-width: 559px) {
+  a {
+    top: 110px;
+  }
+
+  .prject_row {
+    height: 1900px;
+  }
+}
+
+@media (max-width: 425px) {
+  .intro_title h1 {
+    font-size: 3rem;
+  }
+
+  .intro_title p {
+    padding-left: 0;
+  }
+}
+
+@media (max-width: 427px) {
+  a {
+    top: 140px;
+  }
+
+  .prject_row {
+    height: 2030px;
+  }
+}
+
+@media (max-width: 370px) {
+  a {
+    top: 160px;
+  }
+
+  .prject_row {
+    height: 2200px;
+  }
+}
+
+@media (max-width: 307px) {
+  a {
+    top: 180px;
+  }
+
+  .prject_row {
+    height: 2430px;
   }
 }
 </style>
